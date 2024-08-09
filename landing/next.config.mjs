7525372f86d1207c1d710f784a4476e2f28e05a7
tommy <t.johnston141x@gmail.com>
2024-08-09 14:30:47 -0400

    Update next.config.mjs

--- landing/next.config.mjs
+++ landing/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: "/os-status/",
-  basePath: "/os-status/",
+  assetPrefix: "/os-status",
+  basePath: "/os-status",
   images: {
     unoptimized: true,
     remotePatterns: [
